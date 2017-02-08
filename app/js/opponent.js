b10a80451b074a56a9c421f0ea4a157c8487631d
Liam Brennan <liam@anywhich.com>
2017-02-09 07:57:12 +1100

    really smooth online

--- app/js/opponent.js
+++ app/js/opponent.js
@@ -9,8 +9,8 @@ export default class Opponent extends Tank {
   update (data) {
     this.Matter.Body.setPosition(this.body, data.position)
     this.Matter.Body.setAngle(this.body, data.rotation)
-    this.rotateAroundPoint(data.gunRotation, {x: this.body.position.x, y: this.body.position.y})
     this.Matter.Body.setPosition(this.turret, {x: this.body.position.x, y: this.body.position.y + 25})
+    this.rotateAroundPoint(data.gunRotation, data.position)
     this.Matter.Body.setPosition(this.circle, {x: this.body.position.x, y: this.body.position.y})
   }
 }
--- app/js/tank.js
+++ app/js/tank.js
@@ -12,7 +12,7 @@ export default class Tank {
       bullet: 0x0002
     }
 
-    this.firebaseDelay = 500
+    this.firebaseDelay = 50
     this.firebaseCounter = 0
 
     this.bullets = []
